ec664b699cb65af8dcb0bab46d7eed7e2c9cde6b
Sadid Rafan <sadidrafan094@gmail.com>
2025-12-19 15:31:30 +0600

    commit one

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main{
+
+    public static void main(String[] args) {
+       
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("heqqq");
+    }
+
+}
